1bb64e95998077ea32a86d7e3c164785f3338e99
Michael A Nowiszewski <micski@gmail.com>
2018-02-03 22:20:40 +0000

    Update 1.14 - level 1 <break>

--- api/routes/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var ctrlHotels = require('../controllers/hotels.controllers.js');
-router.route('/hotels').get(ctrlHotels.hotelsGetAll);
-router.route('/hotels/:hotelId').get(ctrlHotels.hotelsGetOne);
-module.exports = router;
