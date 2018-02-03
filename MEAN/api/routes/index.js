ed799fa234d2f466a33c36b0f50d73c3b41ba18a
Michael A Nowiszewski <micski@gmail.com>
2018-02-03 22:02:36 +0000

    Update 1.12

---
+++ MEAN/api/routes/index.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var router = express.Router();
+var ctrlHotels = require('../controllers/hotels.controllers.js');
+router.route('/hotels').get(ctrlHotels.hotelsGetAll);
+module.exports = router;
--- MEAN/app.js
+++ MEAN/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var routes = require('./routes');
+var routes = require('./api/routes');
 // Define the port to run on
 app.set('port', 3000);
 // Add middleware to console log every request
