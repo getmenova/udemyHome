99e2d2f635f0897f76cf7de33eebe28effc6eefc
Michael A Nowiszewski <micski@gmail.com>
2018-02-03 21:53:39 +0000

    Update 1.11

--- MEAN/app.js
+++ MEAN/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var routes = require('./routes');
 // Define the port to run on
 app.set('port', 3000);
 // Add middleware to console log every request
@@ -11,16 +12,7 @@ app.use(function(req, res, next) {
 // Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
 // Add some routing
-app.get('/json', function(req, res) {
-	console.log("GET the json");
-	res.status(200).json({
-		"jsonData": true
-	});
-});
-app.get('/file', function(req, res) {
-	console.log("GET the file");
-	res.status(200).sendFile(path.join(__dirname, 'app.js'));
-});
+app.use('/api', routes);
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
 	var port = server.address().port;
